298517f2002c2b373412dfa062af894396b0e64a
Timofey Tychkov <timofeytychkov@gmail.com>
2024-02-01 17:39:40 +0700

    Реализовал простой метод сортировки (сортировка выбором).

---
+++ Functions.h
@@ -0,0 +1,5 @@
+#pragma once
+#include <iostream>
+
+// ѕростой метод сортировки (сортировка выбором)
+void selectionSort(char arr[], int n);
--- Main.cpp
+++ Main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include <Windows.h>
+#include "Functions.h"
+
+using namespace std;
 
 int main()
 {
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
 
+    char arr[] = { 'd', 'b', 'a', 'c', 'e' };
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Исходный массив: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    selectionSort(arr, n);
+
+    cout << "Отсортированный массив: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
 }
